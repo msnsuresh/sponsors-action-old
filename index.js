dcdc287152e88293015bc9995e2e2757d0f3c31f
Suresh Neelakandan <sureshneelakandan@Sureshs-MacBook-Pro.local>
2020-09-02 16:16:05 +0530

    Init commit for sponsors-action

---
+++ index.js
@@ -0,0 +1,48 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+
+(async () => {
+  try {
+    const token = process.env["GITHUB_TOKEN"];
+
+    if (!token) {
+      throw "Token not found";
+    }
+
+    const octokit = new github.GitHub(token);
+    const nwo = process.env["GITHUB_REPOSITORY"] || "/";
+    const [owner, repo] = nwo.split("/");
+    console.log(nwo);
+
+    const readme = await octokit.request(`GET /repos/${owner}/${repo}/readme`, {
+      headers: {
+        authorization: `token ${token}`,
+      },
+    });
+
+    if (readme.headers.status === "404") {
+      console.log("readme not added");
+      return;
+    }
+
+    console.log(readme);
+
+    const sponsors_list = await octokit.request(
+      `GET /repos/${owner}/${repo}/sponsors`,
+      {
+        headers: {
+          authorization: `token ${token}`,
+        },
+      }
+    );
+
+    if (sponsors_list.headers.status === "404") {
+      console.log("sponsors files not added");
+      return;
+    }
+
+    console.log(sponsors_list);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+})();
